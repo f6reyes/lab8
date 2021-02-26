9edcd5fda5871e5fc898df956732fa844d876a92
Fernando Reyes Jr <f6reyes@ucsd.edu>
2021-02-25 18:11:45 -0800

    The holding button doesnt seem to work?

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = "x0F9uyajA2HzvtooDvavOAX34Gb18HlJ";
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map("map", {
+    center: [32.87895924751261, -117.23815129612396],
+    layers: L.mapquest.tileLayer("map"),
+    zoom: 12,
+    zoomControl: false,
+  });
+
+  L.marker([32.87895924751261, -117.23815129612396]).addTo(map);
 }
